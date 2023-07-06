de594e2f9d92edf2a5387e03983bc2e9b7585762
attahchisom6 <attahchisom6@gmail.com>
2023-07-06 21:54:58 +0000

    return function that multiplies a fooat

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,12 +1,42 @@
-def canUnlockAll(boxes):
-    n = len(boxes)
-    visited = [False] * n  # Keeps track of visited boxes
-    visited[0] = True  # Mark the first box as visited
-    stack = [0]  # Stack to perform DFS
-    while stack:
-        box = stack.pop()
-        for key in boxes[box]:
-            if key < n and not visited[key]:
-                visited[key] = True
-                stack.append(key)
-    return all(visited)
+#!/usr/bin/python3
+"""A python module that determines if all boxes can be opened
+   from a list of lists
+"""
+
+
+def canUnlockAll(boxes=[]):
+    """A function that returns True of all box in
+    boxes can be opend
+    """
+    if not boxes:
+        return False
+
+    keys = set([0])
+    for box_id, box in enumerate(boxes):
+        for key in box:
+            if key < len(boxes) and key != box_id:
+                keys.add(key)
+
+    if len(keys) != len(boxes):
+        return False
+
+    return True
+
+
+if __name__ == '__main__':
+    boxes = [
+                [1, 3],
+                [2],
+                [3, 0],
+                [1, 2, 3],
+            ]
+    print(unlockBoxes(boxes))
+
+    boxes = [[1], [2], [3], [4], []]
+    print(canUnlockAll(boxes))
+
+    boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+    print(unlockBoxes(boxes))
+
+    boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+    print(unlockBoxes(boxes))
