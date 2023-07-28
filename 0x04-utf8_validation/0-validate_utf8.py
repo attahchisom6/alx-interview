fce9d2a2ea714f1f9897833fb3f6b7c8483a16c6
attahchisom6 <attahchisom6@gmail.com>
2023-07-28 17:54:30 +0100

    check if the list is utf-8 encodeable

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -23,15 +23,15 @@ def validUTF8(data: List[int]) -> bool:
                 # then its a single byte character
                 num_bytes = 0
 
-            elif num >> 5 == b'110':
+            elif num >> 5 == 0b110:
                 # then its a 2 byte code
                 num_bytes = 1
 
-            elif num >> 4 == b'1110':
+            elif num >> 4 == 0b1110:
                 # its a 3 byte code point
                 num_bytes = 4
 
-            elif num >> 3 == b'11110':
+            elif num >> 3 == 0b11110:
                 # its a 4 byte code point
                 num_bytes = 3
 
@@ -40,7 +40,7 @@ def validUTF8(data: List[int]) -> bool:
 
         # check if the continuation byte havr 10 as its initial
         else:
-            if num >> 6 != b'10':
+            if num >> 6 != 0b10:
                 return False
             num_bytes -= 1
 
