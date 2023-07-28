8cffdfa656c0d552930f65c8ccc002a202d798b3
attahchisom6 <attahchisom6@gmail.com>
2023-07-28 10:02:50 +0000

    tesr

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,36 +1,36 @@
 #!/usr/bin/python3
-"""UTF-8 Validation Module
-"""
+
+""" Determines if a given data set represents a valid UTF-8 encoding """
+
+
+def reverse_bits(num):
+    """ reverses bits and returns list of bits """
+
+    bits = []
+
+    while num:
+        bit = num & 1
+        bits.append(bit)
+        num >>= 1
+
+    bits.reverse()
+    return bits
+
+
+def isASCII(num):
+    """ checks if number is ASCII encoded """
+
+    if num < 128:
+        return True
+
+    return False
 
 
 def validUTF8(data):
-    """Determines if a given data set represents a valid UTF-8 encoding.
+    """ determines if a given data set represents a valid UTF-8 encoding """
 
-    Args:
-        data (list): list of integers
+    for x in data:
+        if not isASCII(x):
+            return False
 
-    Returns:
-        bool: True if data is a valid UTF-8 encoding, else return False
-    """
-    n_bytes = 0
-
-    for num in data:
-        byte = num & 0xff
-        if n_bytes == 0:
-            mask1 = 1 << 7
-            mask2 = 1 << 6
-            while mask1 & byte:
-                n_bytes += 1
-                mask1 = mask1 >> 1
-                mask2 = mask2 >> 1
-            if n_bytes == 0:
-                continue
-            if n_bytes == 1 or n_bytes > 4:
-                return False
-        else:
-            mask1 = 1 << 7
-            mask2 = 1 << 6
-            if not (byte & mask1 and not (byte & mask2)):
-                return False
-        n_bytes -= 1
-    return n_bytes == 0
+    return True
