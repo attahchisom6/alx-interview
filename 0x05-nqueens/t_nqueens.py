e89fcbf7578158ad699ff24186308421f12ab73f
attahchisom6 <attahchisom@gmail.com>
2023-08-05 14:10:41 +0100

    add

--- 0x05-nqueens/t_nqueens.py
+++ 0x05-nqueens/t_nqueens.py
@@ -12,8 +12,9 @@ global queen = 1
 
 def is_safe(board, row, col, N):
     """
-    check if the current position (row, col)  is safe before inserting a queen, that is if inserting a queen at (row, col)
-    will not cause an attack on other queens
+    check if the current position (row, col)  is safe before inserting
+    a queen, that is if inserting a queen at (row, col)
+    will not cause an attack on other previous queens
     Args:
         Board (List): an N x N list of Lisf integers
         row (int): a list of integers, qn entry in board
@@ -39,6 +40,21 @@ def is_safe(board, row, col, N):
     return True
 
 
-def solve_nqueeen(board, row, col):
+def solve_nqueeen(board, row, N):
     """
-    This 1l
+    This will attempt to find a solution to the nqueens puzzle
+    by checking to find the best position to insert a queen
+    if none if found it uses backtracking algorith to redo
+    the previous insertion, then try another solution
+    """
+    # base condition: all queens were inserted
+    if row == N:
+        print_board(board)
+        return True
+
+    for col in row:
+        # if its safe to insert to insert a queen, insert it
+        # and move onto the next row
+        if is_safe(board, col, row, N):
+            board[row][col] = queen
+            solve_nqueens(board, row + 1, N)
