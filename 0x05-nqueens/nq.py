7bfc8faacb4833ff46f6b436fc77494998e588b1
attahchisom6 <attahchisom@gmail.com>
2023-08-05 13:44:20 +0100

    add

--- 0x05-nqueens/nq.py
+++ 0x05-nqueens/nq.py
@@ -18,29 +18,67 @@ def is_anAttackQueen(board, row, col):
     for queens_row, queens_col in enumerate(board):
         # check if the queens are in the same row
         if queens_row == row:
-            return true
+            return True
         # check if they are in the same column
-        if queens_col= col:
+        if queens_col == col:
             return True
         # check if they are within a diagonal line
         if abs(queens_row - row) == abs(queens_col - col):
             return True
-    return  False
+    return False
 
 
-def solve_nqueens(board, row):
+def solve_nqueens(board, row, n):
     """
     this will solve the problem of nqueen using backtracking
     """
     n = len(board)
     if n == row:
+        print_board(board)
         return True
 
-    for col in row:
-        if not is_anAttackQuen(board, row, col):
-            board.append(col)
+    for col in range(n):
+        if not is_anAttackQueen(board, row, col):
+            board[row] = col
             print_nqueens(board)
-         if solve_nqueens(board, col + 1):
-             return True
-         board.pop()
+        solve_nqueens(board, col + 1):
     return False
+
+
+def nqueens(n):
+    """
+    solves te nqueen problemm with the given value of n
+    """
+    # we first insert queens starting from the first row
+    board = []
+    if solve_nqueens(board, 0):
+        print_board(board)
+
+
+def print_board(board):
+    print([[col for col in rows] for rows in board])
+
+
+def main():
+    """
+    execution entry point
+    """
+    args = sys.argv
+    if len(args) < 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        N = int(args[1])
+
+        if N < 4:
+            print("N must be at least 4")
+            sys.exit(1)
+        nqueens(N)
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
