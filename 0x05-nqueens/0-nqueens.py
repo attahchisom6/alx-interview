2572b4d3462a691db198475a67007868419a2e42
attahchisom6 <attahchisom@gmail.com>
2023-08-06 15:09:23 +0100

    testing

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,97 +1,127 @@
 #!/usr/bin/python3
-"""0-nqueens module that solves the N queens problem
+"""
+This module creates and solves the nqueens logic, which is a puzze in which
+nquens are placed in an nxn matrix (chessboard) in such a way that no queen
+interfares or attacks the other queen,that
+is no two queen are in the same row, column or diagonal
 """
 import sys
 
 
-def nqueens(num):
-    """nqueens function that solves the N queens problem
+global queen
+queen = 1
 
-    Args:
-        num (int): number of queens
+
+def is_safe(board, row, col, N):
     """
-    board = [[0 for x in range(num)] for y in range(num)]
-    solve_nqueens(board, 0, num)
-
-
-def solve_nqueens(board, col, num):
-    """solve_nqueens function that solves the N queens problem
-
+    check if the current position (row, col)  is safe before inserting
+    a queen, that is if inserting a queen at (row, col)
+    will not cause an attack on other previous queens
     Args:
-        board (list): board list
-        col (int): column number
-        num (int): number of queens in board
-
-    Returns:
-        bool: True if safe, False otherwise
+        Board (List): an N x N list of Lisf integers
+        row (int): a list of integers, qn entry in board
+        col (int): a single entry in row
+        N: size of the Board
     """
-    if col == num:
-        print_board(board)
-        return True
-
-    res = False
-    for i in range(num):
-        if is_safe(board, i, col, num):
-            board[i][col] = 1
-            res = solve_nqueens(board, col + 1, num) or res
-            board[i][col] = 0
-    return res
-
-
-def is_safe(board, row, col, num):
-    """is_safe function that checks if a queen can be placed on board
-
-    Args:
-        board (list): board list
-        row (int): row number
-        col (int): column number
-        num (int): number of queens in board
-
-    Returns:
-        bool: True if safe, False otherwise
-    """
-    safe_row = row
-    safe_col = col
-    while safe_col >= 0 and safe_row < num:
-        if board[safe_row][safe_col] == 1:
+    # for k in col will check from 0 to col - 1, effectively checking the
+    # previous cols
+    for k in range(col):
+        # check for column safety
+        if board[row][k] == queen:
             return False
-        safe_row += 1
-        safe_col -= 1
 
-    safe_row = row
-    safe_col = col
-    while safe_row >= 0 and safe_col >= 0:
-        if board[safe_row][safe_col] == 1:
+    # check for Top-Right to Bottom-Left diagonal indexes
+    TR_ROW = range(row, -1, -1)
+    TR_COL = range(col, -1, -1)
+    for row_diag, col_diag in zip(TR_ROW, TR_COL):
+        if board[row_diag][col_diag] == queen:
             return False
-        safe_row -= 1
-        safe_col -= 1
 
-    for i in range(col):
-        if board[row][i] == 1:
+    # check for Top-Left to Bottom-Right diagonal
+    TL_ROW = range(row, N, 1)
+    TL_COL = range(col, -1, -1)
+    for row_diag, col_diag in zip(TL_ROW, TL_COL):
+        if board[row_diag][col_diag] == queen:
             return False
     return True
 
 
-def print_board(board):
-    """print_board function that prints the board
-
-    Args:
-        board (list): board list
+def solve_nqueens(board, col, N):
     """
-    print([[i, j] for i in range(
-        len(board)) for j in range(len(board)) if board[i][j] == 1])
+    This will attempt to find a solution to the nqueens puzzle
+    by checking to find the best position to insert a queen
+    if none is found it uses backtracking algorith to redo
+    the previous insertion, then try another solution
+    """
+    # base condition: all queens were inserted
+    if col == N:
+        print_board(board)
+        return True
+
+    res = False
+    for row in range(N):
+        # if its safe to insert a queen in column, insert it
+        # and move onto the next row to check if it too has a column
+        # that can accept a queen
+        if is_safe(board, row, col, N):
+            board[row][col] = queen
+            res = solve_nqueens(board, col + 1, N) or res
+            # Backtrack: After attempting to place a queen and exploring
+            # its subsequent columns, reset the current position (row, col) to,
+            # 0 indicating that no queen can be placed here.
+            # This allows the algorithm to explore other possibilities
+            # and configurations.
+            board[row][col] = 0
+
+    return res
 
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
+def nqueens(N):
+    """
+    this function solves the queen to any given number or size of
+    the chessboard
+    """
+    board = [[0 for _ in range(N)] for _ in range(N)]
+    solve_nqueens(board, 0, N)
+    return
+
+
+def print_board(board):
+    """
+    displays list of solutiob to the chessboard game, per line
+    """
+    # range_board = range(len(board))
+    solutions = []
+    for p, row in enumerate(board):
+        solution = []
+        for k, col in enumerate(row):
+            if col == queen:
+                solution.append(p)
+                solution.append(k)
+        solutions.append(solution)
+    print(solutions)
+
+
+def main():
+    """
+    execution entry point
+    """
+    args = sys.argv
+    if len(args) < 2:
         print("Usage: nqueens N")
         sys.exit(1)
+
     try:
-        NUM = int(sys.argv[1])
-        if NUM < 4:
+        N = int(sys.argv[1])
+
+        if N < 4:
             print("N must be at least 4")
             sys.exit(1)
-        nqueens(NUM)
+        nqueens(N)
     except ValueError:
         print("N must be a number")
         sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
