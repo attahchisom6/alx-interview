39d81bc85a3bf979cc29608568d1d0cd8a0a24d5
attahchisom6 <attahchisom@gmail.com>
2023-08-06 14:56:10 +0100

    add

---
+++ 0x05-nqueens/try_another_approach.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+The nqueen problem,  another approach to
+sp;ve the nqueens problem
+"""
+
+
+def solve_nqueens(board, idx, col, left_diag, right_diag):
+    """
+    solves the nqueen problem
+    """
