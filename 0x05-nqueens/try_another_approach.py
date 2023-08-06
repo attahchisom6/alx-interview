ca9464f74a887aaebf1c5c6d32aa4369338d68d0
attahchisom6 <attahchisom@gmail.com>
2023-08-06 16:50:08 +0100

    add

--- 0x05-nqueens/try_another_approach.py
+++ 0x05-nqueens/try_another_approach.py
@@ -4,8 +4,49 @@ The nqueen problem,  another approach to
 sp;ve the nqueens problem
 """
 
+global queen
+queen = 1
 
-def solve_nqueens(board, idx, col, left_diag, right_diag):
+
+def nqueens(N, idx=0, row=[], left_diag=[], right_diag=[]):
     """
-    solves the nqueen problem
+    INSERT queens in valid rows and column
+    """
+    if idx < N:
+        for k in range(N):
+            if k not in row or idx - k not in left_diag \
+                    or idx + k not in right_diag:
+                yield from nqueens(
+                                N,
+                                idx += 1,
+                                row.append(k),
+                                left_diag.append(idx - k),
+                                right_diag.append(idx + k)
+                            )
+            else:
+                yield row
+
+def solve_nqueens(N):
+    """
+    solves the nqueen problem by calling nqueens
+    retrieve the solution and store it in a list
+    then print it
+    """
+    solutions = []
+
+
+    result = nqueens(N, 0)
+    for p, row in enumerate(result):
+        solution = []
+        for k, col in enumerate(row):
+            solution.append(p)
+            solution.append(k)
+        solutions.append(solution)
+
+    print9solutions)
+
+
+def main():
+    """
+    execution entry point
     """
