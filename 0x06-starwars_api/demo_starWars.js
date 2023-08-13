ed92b3d25d0ce9de5c46a067157ab1cbe78c2ee0
attahchisom6 <attahchisom6@gmail.com>
2023-08-13 09:24:16 +0000

    adding

--- 0x06-starwars_api/demo_starWars.js
+++ 0x06-starwars_api/demo_starWars.js
@@ -1,27 +1,29 @@
 #!/usr/bin/node
 
-// this script printcharacter name from the api in the order in which they  fill the response list
-const request = require("request");
+
+// this script prints character name from the api in the order in which they  fill the response list
+
+const request = require('request');
 async function getMovieNames(movieId) {
   const url = `https://swapi.dev/api/films/${movieId}`;
 
   return new Promise((resolve, reject) => {
-    request(url, (error, response, body) => {
-      if (error) {
-        reject(error);
-      }
+    if (error) {
+      reject(error);
+    }
 
+    require(url, (error, response, body) => {
       const data = JSON.parse(body);
       const characters = data.characters;
 
-      const promiseCharacterArray = characters.map(charUrl => {
-        return new Promise((charResolve, charReject) => {
+      const promiseCharacterArray = await characters.map(charUrl => {
+        return new Promise((charReject, charResolve) => {
           request(charUrl, (charError, charResponse, charBody) => {
             if (charError) {
-              charReject(charError);
+              charResolve(charError);
             }
-            const charData = JSON.parse(charBody)
-            resolve(charData.name)
+            charData = JSON.parse(body);
+            charResolve(charData.name);
           });
         });
       });
@@ -35,18 +37,18 @@ async function getMovieNames(movieId) {
       });
     });
   });
-};
+}
 
 const movieId = process.argv[2]
 if (!movieId) {
   console.log(`Usage: node ${process.argv[1]} <number>`);
 } else {
-  getMovieNames(movieId)
-  .then(characterNames => {
+  await getMovieNames(movieId)
+    .then(characterNames => {
     characterNames.forEach(name => {
       console.log(name);
-    })
-  })
+    });
+  });
   .catch(error => {
     console.error(error);
   });
