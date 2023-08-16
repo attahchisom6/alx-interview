5f9e5eda9c74a30519f0171e7e542eccc4c24b26
attahchisom6 <attahchisom@gmail.com>
2023-08-16 22:22:20 +0100

    rotating a matrix 90 degrees clockwisely

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -4,6 +4,7 @@ A program to rotate a matrix 90 degree clockwisely, we achieve this by
 1. interate through the row and column of the matrix, then set the element at
 row p and column k to be at row k and
 columb p, the element at row k and column p to be at row p and column k
+thus effectively reflecting the element at each side of the diagonal
 
 2. we iterate through each row in the natrix and reverse it
 """
