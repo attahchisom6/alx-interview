f38906ca8804154526e71f5fb48469514a448077
attahchisom6 <attahchisom6@gmail.com>
2023-08-31 12:52:29 +0000

    added

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -22,14 +22,3 @@ def island_perimeter(grid):
                 else:
                     perimeter -= 2
     return perimeter
-
-
-if __name__ == "__main__":
-    grid = [
-        [0, 0, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 1, 1, 0, 0],
-        [0, 0, 0, 0, 0, 0]
-    ]
-    print(island_perimeter(grid))
