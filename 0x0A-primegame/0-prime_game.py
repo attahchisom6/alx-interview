1fade60a0103660de4149d39c83de4031975f91c
attahchisom6 <attahchisom6@gmail.com>
2023-09-07 15:34:49 +0000

    changed

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -20,7 +20,7 @@ def isWinner(x, nums):
         if num <= 1:
             return False
 
-        for k in range(2, int(n ** 0.5) + 1):
+        for k in range(2, n + 1):
             if num % k == 0:
                 return False
         return True
